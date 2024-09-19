e84c5c1dac78ab5b7f713318c516c331b8435c13
KinnaGt <devilboogey@gmail.com>
2024-09-19 15:34:03 -0300

    Add b)i)

--- code/part_1.py
+++ code/part_1.py
@@ -5,7 +5,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
-
 # Cargar el dataset
 df = pd.read_csv("code/dataset/FIFA21.csv")
 
@@ -137,12 +136,15 @@ x0 = x.mean()  # Característica fija, puedes cambiar a otro valor específico
 y0_hat = b0_hat + b1_hat * x0
 
 # Incertidumbre de predicción
-pred_interval = t_alpha_half * sqrt(sigma_hat_sqrd * (1 + 1 / len(x) + (x0 - x_avg)**2 / Sxx))
+pred_interval = t_alpha_half * \
+    sqrt(sigma_hat_sqrd * (1 + 1 / len(x) + (x0 - x_avg)**2 / Sxx))
 
 # Intervalo de confianza para la media
-conf_interval = t_alpha_half * sqrt(sigma_hat_sqrd / len(x) + (x0 - x_avg)**2 / Sxx)
+conf_interval = t_alpha_half * \
+    sqrt(sigma_hat_sqrd / len(x) + (x0 - x_avg)**2 / Sxx)
 
-# Calcular la proporción de veces que el valor de mercado supera la incertidumbre de predicción
+# Calcular la proporción de veces que el valor de mercado supera la
+# incertidumbre de predicción
 exceeds_prediction_interval = np.sum(
     (y > (y0_hat + pred_interval)) | (y < (y0_hat - pred_interval)))
 proportion_exceeds = exceeds_prediction_interval / len(y)
--- code/part_2.py
+++ code/part_2.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 
+# * b) Ecuación para predecir el valor de mercado del jugador
+# * a partir de varias características.
+
 # Cargar el dataset
 df = pd.read_csv("code/dataset/FIFA21.csv")
 
@@ -11,7 +14,13 @@ x2_key = 'potential'
 df = df[[y_key, x1_key, x2_key]].dropna()
 
 # Filtrar datos con overall mayor a 65
-# df = df[df["overall"] > 65]
+df = df[df["overall"] > 65]
+# df = df[df["potential"] > 65]
+
+# * i) Usando el método de mínimos cuadrados. Explica los indicadores obtenidos
+# *  (como el coeficiente de determinación y la correlación) y proporciona una
+# * breve interpretación de los resultados
+
 
 # Definir las variables
 x1 = df[x1_key].values
@@ -21,6 +30,8 @@ y = df[y_key].values
 x1_sum = x1.sum()
 x2_sum = x2.sum()
 y_sum = y.sum()
+
+print(y.mean())
 
 # Calcular la suma de los cuadrados de x1 y x2
 x1_square = (x1 ** 2).sum()
@@ -52,8 +63,8 @@ b2 = beta[2]  # Coeficiente de x2
 
 # Mostrar los coeficientes de la regresión
 print(f"Coeficiente b0 (intersección): {b0}")
-print(f"Coeficiente b1 (PesoIX1): {b1}")
-print(f"Coeficiente b2 (PesoAX2): {b2}")
+print(f"Coeficiente b1 (Overall): {b1}")
+print(f"Coeficiente b2 (Age): {b2}")
 
 # Predicción usando la ecuación de regresión múltiple
 y_hat = b0 + b1 * x1 + b2 * x2
@@ -62,3 +73,28 @@ y_hat = b0 + b1 * x1 + b2 * x2
 print(f"Predicciones: {y_hat[:5]}")  # Mostrar solo las primeras 5 predicciones
 # Imprimir la ecuación de regresión múltiple
 print(f"Ecuación de regresión múltiple: y_hat = {b0} + {b1}*x1 + {b2}*x2")
+
+
+# Calcular R^2
+ss_res = np.sum((y - y_hat) ** 2)  # Suma de los residuos al cuadrado
+ss_tot = np.sum((y - y.mean()) ** 2)  # Suma total de los cuadrados
+r2 = 1 - (ss_res / ss_tot)
+
+print(f"Coeficiente de determinación (R²): {r2}")
+
+corr_overall = np.corrcoef(x1, y)[0, 1]
+corr_potential = np.corrcoef(x2, y)[0, 1]
+
+print(f"Correlación entre overall y value_eur: {corr_overall}")
+print(f"Correlación entre potential y value_eur: {corr_potential}")
+
+
+# * ii) Usando el método de descenso por gradiente. ¿Son los valores obtenidos iguales a los
+# * conseguidos mediante la resolución del sistema de ecuaciones normales? Muestra los
+# * resultados obtenidos junto con las últimas iteraciones del algoritmo. Indica los valores de los
+# * parámetros utilizados (como tasa de aprendizaje y número de iteraciones)
+
+
+# * iii) Da una interpretación del criterio de corte utilizado en el algoritmo del gradiente. Explica
+# * si presenta alguna falla. Si no es una buena condición de corte, ¿puedes sugerir un criterio
+# * alternativo más eficaz?
